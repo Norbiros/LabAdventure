fe692c55d5ed444205a0a523510d2ea834773045
Norbiros <norbiros@protonmail.com>
2023-02-26 21:22:36 +0100

    Better map, and better audio!

--- AudioStreamPlayer.gd
+++ AudioStreamPlayer.gd
@@ -1,8 +1,8 @@
 extends AudioStreamPlayer
 
+var rng = RandomNumberGenerator.new()
 var songs = [
 	load("res://Sounds/medieval_loop.wav"),
-	
 	load("res://Sounds/Shepherd_dog.wav"),
 	load("res://Sounds/gem_kings_gallery.mp3"),
 	load("res://Sounds/pretty-maiden-mageonduty.mp3")
@@ -13,12 +13,11 @@ func _ready():
 	next_song()
 
 func next_song():
+
+	rng.randomize()
+	var i = rng.randf_range(0, len(songs))
 	self.stream = songs[i]
 	play(0.0)
-	if i + 1 >= len(songs):
-		i = 0
-	else:
-		i += 1
 
 func _on_AudioStreamPlayer_finished():
 	next_song()
