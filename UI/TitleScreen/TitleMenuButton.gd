3731b6cf5ed1fd3e8f01e8b40b5980c37730e4f3
Norbiros <norbiros@protonmail.com>
2023-02-27 15:34:23 +0100

    Textures!

--- UI/TitleScreen/TitleMenuButton.gd
+++ UI/TitleScreen/TitleMenuButton.gd
@@ -2,10 +2,12 @@ extends Control
 
 
 export(String) var button_name = ""
+export(bool) var flip = false
 onready var menuButton = $TitleMenuButton/Label
 
 func _ready():
 	menuButton.text = button_name
+	$TitleMenuButton.flip_h = flip
 
 func _on_TitleMenuButton_button_down():
 	Global.emit_signal("pressed_menu_button", button_name)
