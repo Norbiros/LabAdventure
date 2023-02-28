074ff5e0297c489defa03e9de4be5490f95308cd
EpicPlayerA10 <adasko.lesiak@gmail.com>
2023-02-28 22:30:42 +0100

    The Rock

--- World/Stone.gd
+++ World/Stone.gd
@@ -2,9 +2,11 @@ extends Area2D
 
 var interacted = false
 
+var items = ["P.tres", "Sulfur.tres", "IronOre.tres"]
+
 func _on_Stone_body_entered(body):
 	if body.name  == "Player" and interacted == false:
-		Global.player_interactions[self.name] = ["Kliknij, SPACJĘ aby podnieść!", self, "run_interaction"]
+		Global.player_interactions[self.name] = ["Kliknij, F aby podnieść!", self, "run_interaction"]
 
 func run_interaction():
 	interacted = true
@@ -16,9 +18,18 @@ func run_interaction():
 	yield(t, "timeout")
 	t.queue_free()
 	
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	
+	print(items)
+	print(items.size() - 1)
+	print(rng.randi_range(0, items.size() - 1))
+	var item = items[rng.randi_range(0, items.size() - 1)]
+	print(item)
+	
 	var car_resource = preload("res://Util/ItemDrop.tscn")
 	var car = car_resource.instance()
-	car.resource = "res://Items/Resources/IronOre.tres"
+	car.resource = "res://Items/Resources/"+item
 	car.position = position
 	car.z_index = 10
 	get_owner().call_deferred("add_child", car)
@@ -28,3 +39,21 @@ func run_interaction():
 func _on_Stone_body_exited(body):
 	if body.name  == "Player":
 		Global.player_interactions.erase(self.name)
+		
+		
+func list_files_in_directory(path):
+	var files = []
+	var dir = Directory.new()
+	dir.open(path)
+	dir.list_dir_begin()
+
+	while true:
+		var file = dir.get_next()
+		if file == "":
+			break
+		elif not file.begins_with("."):
+			files.append(file)
+
+	dir.list_dir_end()
+
+	return files
