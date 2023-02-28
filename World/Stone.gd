14f2584e93151f7c512e8288e41ab558e0f690e3
EpicPlayerA10 <adasko.lesiak@gmail.com>
2023-02-28 22:31:35 +0100

    Cleanup

--- World/Stone.gd
+++ World/Stone.gd
@@ -21,11 +21,7 @@ func run_interaction():
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
 	
-	print(items)
-	print(items.size() - 1)
-	print(rng.randi_range(0, items.size() - 1))
 	var item = items[rng.randi_range(0, items.size() - 1)]
-	print(item)
 	
 	var car_resource = preload("res://Util/ItemDrop.tscn")
 	var car = car_resource.instance()
@@ -39,21 +35,3 @@ func run_interaction():
 func _on_Stone_body_exited(body):
 	if body.name  == "Player":
 		Global.player_interactions.erase(self.name)
-		
-		
-func list_files_in_directory(path):
-	var files = []
-	var dir = Directory.new()
-	dir.open(path)
-	dir.list_dir_begin()
-
-	while true:
-		var file = dir.get_next()
-		if file == "":
-			break
-		elif not file.begins_with("."):
-			files.append(file)
-
-	dir.list_dir_end()
-
-	return files
