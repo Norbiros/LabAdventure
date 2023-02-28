a033dc8003388ed5eec5ba9aac55c27751039bb5
Norbiros <norbiros@protonmail.com>
2023-02-28 08:53:04 +0100

    More work...

--- Scenes/TutorialLevel/TutorialLevel.gd
+++ Scenes/TutorialLevel/TutorialLevel.gd
@@ -1,16 +1,7 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	var gui = load("res://UI/HUD/GUI/GUI.tscn").instance()
+	add_child(gui)
+	GameSaver.load_level()
+	Global.emit_signal("teleported_in")
